c60a46b69422a5963da60f0d6c053c4f8a01088f
Geovic Fagel <geo@Geovics-MacBook-Pro.local>
2019-07-10 23:07:54 +0800

    added components social icons

---
+++ src/components/SocialIcons/SocialIcons.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './style.scss';
+
+const SocialIcons = (props, context) => {
+  const { theme: { colorPrimary } } = context;
+  
+  return (
+    <div className="social-icons animate-icons">
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/Geovica" style={ { color: colorPrimary } }><i className="fab fa-github"></i></a>
+        <a target="_blank" rel="noopener noreferrer" href="#" style={ { color: colorPrimary } }><i className="fab fa-twitter"></i></a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/geovic-fagel-8667aa171/" style={ { color: colorPrimary } }><i className="fab fa-linkedin"></i></a>
+        <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@geolegaf" style={ { color: colorPrimary } }><i className="fab fa-medium"></i></a>
+
+      </div>
+  );
+};
+
+SocialIcons.contextTypes = {
+  theme: PropTypes.any
+};
+
+export default SocialIcons;
