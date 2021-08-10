e5bad6c8eecd724b530a23cc951904ce014ba999
Geovica <fgeovic@gmail.com>
2021-08-10 22:14:47 +0800

    updating linkedin

--- src/components/SocialIcons/SocialIcons.js
+++ src/components/SocialIcons/SocialIcons.js
@@ -10,7 +10,7 @@ const SocialIcons = (props, context) => {
     <div className="social-icons animate-icons">
         <a target="_blank" rel="noopener noreferrer" href="https://github.com/Geovica" style={ { color: colorPrimary } }><i className="fab fa-github"></i></a>
         <a target="_blank" rel="noopener noreferrer" href="#" style={ { color: colorPrimary } }><i className="fab fa-twitter"></i></a>
-        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/geovic-fagel-8667aa171/" style={ { color: colorPrimary } }><i className="fab fa-linkedin"></i></a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/geovic-fagel-5769aa212/" style={ { color: colorPrimary } }><i className="fab fa-linkedin"></i></a>
         <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@geolegaf" style={ { color: colorPrimary } }><i className="fab fa-medium"></i></a>
 
       </div>
